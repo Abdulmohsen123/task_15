06a5fbf2d5040a59b03db25154b48b144ad05372
Abdulmohsen <training.coded@gmail.com>
2018-07-29 13:01:23 +0300

    some of the task are completed

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,52 @@
 from rest_framework import serializers
 from restaurants.models import Restaurant
+from django.http import JsonResponse, Response
 
 class RestaurantListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = [
-        	'name',
+			'id',
+        	'owner',
+			'name',
+        	'description',
         	'opening_time',
         	'closing_time',
         	]
+
+class RestaurantRetreiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = [
+			'id',
+        	'owner',
+			'name',
+        	'description',
+        	'opening_time',
+        	'closing_time',
+        	]
+
+class RestaurantUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = [
+			'id',
+        	'owner',
+			'name',
+        	'description',
+        	'opening_time',
+        	'closing_time',
+        	]
+
+	def update(self, request, *args, **kwargs):
+		instance = self.get_object()
+		instance.name = request.data.get('name')
+		instance.description = request.data.get('description')
+		instance.opening_time = request.data.get('opening_time')
+		instance.closing_time = request.data.get('closing_time')
+		instance.save()
+
+		serializer = self.get_serializer(instance)
+		serializer.is_valid(raise_exception=True)
+		self.perform_update(serializer)
+		return Response(serializer.data)
--- api/views.py
+++ api/views.py
@@ -1,20 +1,30 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from restaurants.models import Restaurant
 from .serializers import RestaurantListSerializer
 
+
+
 class RestaurantListView(ListAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantListSerializer
+
+# Complete me
+class RestaurantDetailView(RetrieveAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantListSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'object_id'
+
+
+# Complete me
+class RestaurantUpdateView(UpdateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantListSerializer
+
+
+# Complete me
+class RestaurantDeleteView(DestroyAPIView):
 	queryset = Restaurant.objects.all()
 	serializer_class = RestaurantListSerializer
-
-# Complete me
-class RestaurantDetailView():
-
-
-
-# Complete me
-class RestaurantUpdateView():
-
-
-
-# Complete me
-class RestaurantDeleteView():
+	lookup_field = 'id'
+	lookup_url_kwarg = 'object_id'
